f5b4e8448520668993953b1ea0d0e99601d4949f
TAN DAT <dattt1848@ut.edu.vn>
2026-05-02 20:05:48 +0700

    feat(media): them tinh nang Tu dong kiem tra va tao bucket trong minio neu chua co

--- media-service/src/main/java/com/lms/media/config/MinioBucketInitializer.java
+++ media-service/src/main/java/com/lms/media/config/MinioBucketInitializer.java
@@ -1,4 +1,50 @@
 package com.lms.media.config;
 
+import com.lms.common.exception.AppException;
+import com.lms.media.exception.MediaErrorCode;
+import io.minio.BucketExistsArgs;
+import io.minio.MakeBucketArgs;
+import io.minio.MinioClient;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
 public class MinioBucketInitializer {
+
+    private final MinioClient minioClient;
+
+    @Value("${minio.bucket-name}")
+    private String bucketName;
+
+    /**
+     * Annotation @PostConstruct giúp hàm này tự động chạy MỘT LẦN DUY NHẤT
+     * ngay khi media-service khởi động lên.
+     */
+    @PostConstruct
+    public void initBucket() {
+        try {
+            // Goi ham kiem tra bucket ton tai chua
+            boolean bucketExists = minioClient.bucketExists(
+                    BucketExistsArgs.builder().bucket(bucketName).build()
+            );
+
+            // Neu bucket chua ton tai thi tao bucket moi
+            if (!bucketExists) {
+                minioClient.makeBucket(
+                        MakeBucketArgs.builder().bucket(bucketName).build()
+                );
+                log.info("Make bucket Successfully");
+            } else {
+                log.info("Bucket already exists");
+            }
+        } catch (Exception e) {
+            log.error("Error while initializing Minio Bucket Initializer", e);
+            throw new AppException(MediaErrorCode.INITIALIZE_BUCKET_MINIO_FAILED);
+        }
+    }
 }
--- media-service/src/main/java/com/lms/media/exception/MediaErrorCode.java
+++ media-service/src/main/java/com/lms/media/exception/MediaErrorCode.java
@@ -1,4 +1,17 @@
 package com.lms.media.exception;
 
-public class MediaErrorCode {
+import com.lms.common.exception.ErrorCode;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum MediaErrorCode implements ErrorCode {
+    INITIALIZE_BUCKET_MINIO_FAILED(4001, "Initialize Minio Bucket Failed", HttpStatus.INTERNAL_SERVER_ERROR),
+    ;
+
+    private final int code;
+    private final String message;
+    private final HttpStatus httpStatus;
 }
