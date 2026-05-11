35fcce05af2fc0f5504129313f593c2d1c6d12ba
TAN DAT <dattt1848@ut.edu.vn>
2026-05-11 15:52:56 +0700

    feat(media): bảo mật internal api

--- media-service/src/main/java/com/lms/media/controller/InternalMediaController.java
+++ media-service/src/main/java/com/lms/media/controller/InternalMediaController.java
@@ -2,6 +2,8 @@ package com.lms.media.controller;
 
 import com.lms.common.dto.response.ApiResponse;
 import com.lms.media.service.MediaService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,12 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/internal/v1")
 @RequiredArgsConstructor
+@Tag(name = "Internal Media Controller", description = "APIs serve internal services")
 public class InternalMediaController {
 
     private final MediaService mediaService;
 
-    @DeleteMapping("/{mediaId}")
-    public ApiResponse<?> deleteFile (@PathVariable String mediaId) {
+    @Operation(summary = "Delete media file", description = "Delete media file from database and minio")
+    @DeleteMapping("media-jobs/lessons/{lessonId}/files")
+    public ApiResponse<?> deleteFile (@PathVariable(name = "lessonId") String mediaId) {
         mediaService.removeFile(mediaId);
         return ApiResponse.success(
                 null,
---
+++ media-service/src/main/java/com/lms/media/security/InternalApiFilter.java
@@ -0,0 +1,67 @@
+package com.lms.media.security;
+
+import com.lms.common.exception.AppException;
+import com.lms.media.exception.MediaErrorCode;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.List;
+
+@Slf4j
+@Component
+public class InternalApiFilter  extends OncePerRequestFilter {
+
+    @Value("${application.security.internal-key}")
+    private String internalKey;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String url = request.getRequestURL().toString();
+        // Kiem tra request ko phai noi bo thi khong xu li tiep
+        if (!url.contains("/api/internal/v1")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        // Neu cua noi bo thi lay secret key de doi chieu
+        String authHeader = request.getHeader("X-Internal-Key");
+        if (authHeader == null ) {
+            // Header ko co thi nem loi 401
+            log.error("Request {} missed header Internal Key", request.getRequestURL());
+            throw new AppException(MediaErrorCode.UNAUTHORIZED, "Internal Key Header Missed");
+        }
+        String secretKey = request.getHeader("X-Internal-Key");
+        if (!authHeader.equals(internalKey)) {
+            // Key sai thi nem loi 401
+            String attackerIp = request.getRemoteAddr();
+            log.error("CẢNH BÁO: Phát hiện truy cập trái phép vào API Internal!");
+            log.error("Địa chỉ IP: {}", attackerIp);
+            log.error("URL yêu cầu: {}", request.getRequestURI());
+            log.error("Thời điểm: {}", LocalDateTime.now());
+            throw new AppException(MediaErrorCode.UNAUTHORIZED, "Invalid Internal Secret Key");
+        }
+
+        // Kiem tra hoan thanh tien hanh tao authentication cho spring security
+        List<SimpleGrantedAuthority> authorities = Collections.singletonList(new SimpleGrantedAuthority("INTERNAL"));
+        UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                "InternalService",
+                null,
+                authorities
+        );
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        filterChain.doFilter(request, response);
+    }
+}
