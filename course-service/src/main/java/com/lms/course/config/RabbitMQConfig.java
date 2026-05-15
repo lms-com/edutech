54e57076b239f9cc46c4d2e1e0f17c1e645453e1
TAN DAT <dattt1848@ut.edu.vn>
2026-05-15 12:07:42 +0700

    feat(course): tích hợp RabbitMQ và kết nối với video.completed.queue trong course-service - thêm thư viện rabbitMQ trong pom - cài đặt cấu hình RabbitMQ trong course-service.yml - thiết lập kết nối trong RabbitMQConfig

--- course-service/src/main/java/com/lms/course/config/RabbitMQConfig.java
+++ course-service/src/main/java/com/lms/course/config/RabbitMQConfig.java
@@ -1,4 +1,28 @@
 package com.lms.course.config;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class RabbitMQConfig {
+
+    public static final String MEDIA_EXCHANGE = "media.exchange";
+    public static final String VIDEO_COMPLETED_QUEUE = "video.completed.queue";
+    public static final String VIDEO_COMPLETED_ROUTING_KEY = "video.completed.routing.key";
+
+    @Bean
+    public Queue videoCompletedQueue() {
+        return new Queue(VIDEO_COMPLETED_QUEUE, true);
+    }
+
+    @Bean
+    public Binding videoCompletedBinding() {
+        return BindingBuilder.bind(videoCompletedQueue())
+                .to(new DirectExchange(MEDIA_EXCHANGE))
+                .with(VIDEO_COMPLETED_ROUTING_KEY);
+    }
 }
