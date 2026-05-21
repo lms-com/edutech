3b97786fdcb8692bf4a19cb73284a3ef8e27de6d
TAN DAT <dattt1848@ut.edu.vn>
2026-05-21 16:50:54 +0700

    feat(order): tạo mới và thiết lập order-service - thêm thư viện trong file pom.xml - tích hợp config-server trong application.yml - tạo file order-service và cấu hình cần thiết cho nó trong config-server

--- video-worker-service/src/main/java/com/lms/worker/service/VideoProcessingWorker.java
+++ video-worker-service/src/main/java/com/lms/worker/service/VideoProcessingWorker.java
@@ -20,7 +20,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.security.SecureRandom;
