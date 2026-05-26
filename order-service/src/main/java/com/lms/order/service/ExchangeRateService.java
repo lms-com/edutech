6eafaa81d257d4f4e0e5d9da61e4b777f5622393
TAN DAT <dattt1848@ut.edu.vn>
2026-05-26 21:19:23 +0700

    feat(order): tạo service lấy tỉ giá mô phỏng để mock data tạm thời

--- order-service/src/main/java/com/lms/order/service/ExchangeRateService.java
+++ order-service/src/main/java/com/lms/order/service/ExchangeRateService.java
@@ -1,4 +1,26 @@
 package com.lms.order.service;
 
+import com.lms.common.exception.AppException;
+import com.lms.order.exception.OrderErrorCode;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+@Service
 public class ExchangeRateService {
+
+    public BigDecimal getRate (String fromCurrency, String toCurrency) {
+        if (fromCurrency.equalsIgnoreCase(toCurrency)) {
+            return BigDecimal.ONE;
+        }
+
+        if (fromCurrency.equalsIgnoreCase("USD") && toCurrency.equalsIgnoreCase("VND"))
+            return BigDecimal.valueOf(25450.00);
+
+        if (fromCurrency.equalsIgnoreCase("VND") && toCurrency.equalsIgnoreCase("USD"))
+            return BigDecimal.ONE.divide(BigDecimal.valueOf(25450.00), 6, RoundingMode.HALF_UP);
+
+        throw new AppException(OrderErrorCode.CURRENCY_NOT_SUPPORTED);
+    }
 }
