ddba30211a06e72baec6367a07d38ea37ddcbd77
TAN DAT <dattt1848@ut.edu.vn>
2026-05-26 21:13:47 +0700

    fix(order): sửa lỗi đặt sai tên hàm trong PromotionRepository

--- order-service/src/main/java/com/lms/order/repository/PromotionRepository.java
+++ order-service/src/main/java/com/lms/order/repository/PromotionRepository.java
@@ -2,10 +2,12 @@ package com.lms.order.repository;
 
 import com.lms.order.model.Promotion;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface PromotionRepository extends JpaRepository<Promotion, String> {
 
-    Optional<Promotion> findByPromotionCode(String promotionCode);
+    Optional<Promotion> findByCode(String promotionCode);
 }
