f3790edbea58765bacefe6b87c470c5d3987d958
TAN DAT <dattt1848@ut.edu.vn>
2026-05-26 21:05:52 +0700

    feat(order): thiết lập bổ sung các cấu hình cần thiết - thêm order vào ds module của project trong file pom.xml - kích hoạt jpa auditing trong file hàm main

--- order-service/src/main/java/com/lms/order/OrderServiceApplication.java
+++ order-service/src/main/java/com/lms/order/OrderServiceApplication.java
@@ -6,10 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableFeignClients
 @EnableDiscoveryClient
+@EnableJpaAuditing
 public class OrderServiceApplication {
 
     public static void main(String[] args) {
---
+++ order-service/src/main/java/com/lms/order/config/SecurityConfig.java
@@ -0,0 +1,40 @@
+package com.lms.order.config;
+
+import com.lms.common.security.HeaderAuthenticationFilter;
+import com.lms.common.security.InternalApiFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Value("${application.security.internal-key}")
+    private String internalKey;
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeRequests(auth -> auth
+                        .requestMatchers("/api/internal/**").hasAnyAuthority("INTERNAL")
+                        .requestMatchers("/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(new HeaderAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(new InternalApiFilter(internalKey), UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+}
